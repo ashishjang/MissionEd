eb0a2c8bbeca115d9f6b2db48c91265882766aab
Ashish <ashishjang@outlook.com>
2021-10-27 23:16:21 +0530

    recommendation changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SimpleForm from "./components/SimpleForm";
 import Recommendation from "./components/Recommedation";
+import SimpleForm from "./components/SimpleForm";
 function App() {
   return (
-    <div><Recommendation/></div>
+    <div><SimpleForm/></div>
   );
 }
 
