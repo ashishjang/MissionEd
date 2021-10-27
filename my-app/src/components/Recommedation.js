e222a703558dc32c3776e5c0c9e7b4211eb71e74
Ashish <ashishjang@outlook.com>
2021-10-27 23:15:35 +0530

    Updated recommendations

--- my-app/src/components/Recommedation.js
+++ my-app/src/components/Recommedation.js
@@ -3,36 +3,45 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button,  Row, Col, Container,Card} from 'react-bootstrap';
 import './Recommendation.css'
 import Image from 'react-bootstrap/Image'
+import { extend } from 'jquery';
 
-class Recommendation extends Component {   
-    render(){
-       return(
-         <Container className="container">
-          <div>
-          <h1 className = 'heading-1'>Recommendations</h1>
-           </div>
-          <Card className = "list-element">
-          <Row>
-            <Col className = 'columns'>
-            Data Science Internship
+const EachItem = () => {   
+       return (
+      <Container>
+        <div>
+      <h1 className = 'heading-1'>Recommendations</h1>
+       </div>
+        <Card className = "list-element">
+        <Row>
+          <Col className = 'columns'>
+          <text className = 'label'>Data Science Internship</text>
+          </Col>
+          <Col className = 'columns'>
+            <Image src = 'abc/png' roundedCircle />
+              </Col>
+              <Col className = 'columns'>
+            MissionEd
+            </Col>
+          <Col className = 'columns'>
+            6 Likes
             </Col>
             <Col className = 'columns'>
-              <Image src= "./logo.svg" roundedCircle />
-              MissionEd
-              </Col>
+            2 weeks
+            </Col>
             <Col className = 'columns'>
-              6 Likes
-              </Col>
-              <Col className = 'columns'>
-              2 weeks
-              </Col>
-              <Col className = 'columns'>
-            <Button variant="primary">View Details</Button>
-              </Col>
-          </Row>
-        </Card>
-           </Container>
-        )    
+          <Button variant="primary">View Details</Button>
+            </Col>
+        </Row>
+      </Card>
+      </Container>
+       )
+}
+
+class Recommendation extends Component {   
+  render(){
+     return(
+       EachItem() 
+     )
 }
 }
 
