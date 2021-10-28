b876b8eb4e8fb135dc90d85c57d1f83e62b0a13d
Ashish <ashishjang@outlook.com>
2021-10-28 15:13:39 +0530

    recommendation

--- my-app/src/components/SimpleForm.js
+++ my-app/src/components/SimpleForm.js
@@ -1,59 +1,95 @@
-import React, {Component } from 'react';
+import React, {Component, useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button,  Row, Form, Col, Container,Card} from 'react-bootstrap';
-import './SimpleForm.css'
-import Select from 'react-select'
+import './SimpleForm.css';
 
-const skills = [
-    { value: '1', label: 'Python' },
-    { value: '2', label: 'Machine Learning' },
-    { value: '3', label: 'R' },
-  ];
 
-class SimpleForm extends Component {
-    state = {
-        selectedOption: null,
-      };
-      handleChange = (selectedOption) => {
-        this.setState({ selectedOption });
-        console.log(`Option selected:`, selectedOption);
-      };     
-    render(){
-    const { selectedOption } = this.state;
+export default function SimpleForm(){
 
+    const [user,setUser] = useState({
+      FirstName:'',
+      LastName:'',
+      Email:'',
+      Password:'',
+      City:"",
+      State:"",
+      Zip: "",
+  });
+  let name, value;
+    const getUserData = (e) =>{
+            name= e.target.name; 
+            value= e.target.value;
+        setUser({
+            ...user, [name]:value
+        })
+    }
+  
+    const postData = async(e) =>{ e.preventDefault();
+      const {FirstName,LastName,Email,Password,Internship,Skills,City,State,Zip} = user;  
+      if(FirstName && LastName && Email && Password && City && State && Zip){ 
+      const res  = await fetch(
+           "https://something-unique-aaedb-default-rtdb.firebaseio.com/database.json", 
+           { 
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+             FirstName,
+             LastName,
+             Email,
+             Password,
+             City,
+             State,
+             Zip
+            }),
+         }); 
+        if (res){
+          setUser({
+            FirstName:'',
+            LastName:'',
+            Email:'',
+            Password:'',
+            City:"",
+            State:"",
+            Zip: "",
+              });
+        alert("Data Stored Successfully")
+        }
+      }else{
+        alert("Please fill all the fields")
+      }
+    }
        return(
        <Container>
-          
     <Card className = 'card'>
-    <div>
+        <div>
            <h1 className = 'heading'><center>Profile Details</center></h1>
            </div>
-    <Form>
+    <Form method = 'POST'>
     <Row className="mb-3">
       <Form.Group as={Col} controlId="formGridName">
         <Form.Label>First Name</Form.Label>
-        <Form.Control type="name" placeholder="Enter First Name" />
+        <Form.Control type="name" name = "FirstName" placeholder="Enter First Name" value = {user.FirstName} onChange = {getUserData}/>
       </Form.Group>
   
       <Form.Group as={Col} controlId="formGridLastName">
         <Form.Label>Last Name</Form.Label>
-        <Form.Control type="last name" placeholder="Enter Last Name" />
+        <Form.Control type="name" placeholder="Enter Last Name" value = {user.LastName} onChange = {getUserData} />
       </Form.Group>
     </Row>
     <Row className="mb-3">
       <Form.Group as={Col} controlId="formGridEmail">
         <Form.Label>E-mail</Form.Label>
-        <Form.Control type="name" placeholder="Enter your e-mail" />
+        <Form.Control type="name" placeholder="Enter your e-mail" value = {user.email} onChange = {getUserData}/>
       </Form.Group>
   
       <Form.Group as={Col} controlId="formGridPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="last name" placeholder="Enter your password" />
+        <Form.Control type="last name" placeholder="Enter your password" value = {user.Password} onChange = {getUserData}/>
       </Form.Group>
     </Row>
     <Form.Group className="mb-3" controlId="formGridAddress1">
       <Form.Label>Internship</Form.Label>
-        <Form.Select defaultValue="Choose...">
+        <Form.Select defaultValue="Choose..." value = {user.Internship} onChange = {getUserData}>
           <option>Choose...</option>
           <option>Data Science</option>
           <option>Web Development</option>
@@ -62,38 +98,39 @@ class SimpleForm extends Component {
   
     <Form.Group className="mb-3" controlId="formGridAddress2">
       <Form.Label>Skills</Form.Label>
-      <Select
-        className = "select"
-        multiple = {true}
-        value={selectedOption}
-        onChange={this.handleChange}
-        options={skills}
-      />
+      <Form.Select defaultValue="Choose..." value = {user.Skills} onChange = {getUserData}>
+          <option>Choose...</option>
+          <option>Python</option>
+          <option>R</option>
+          <option>Machine Learning</option>
+          </Form.Select>
     </Form.Group>
   
     <Row className="mb-3">
       <Form.Group as={Col} controlId="formGridCity">
         <Form.Label>City</Form.Label>
-        <Form.Control />
+        <Form.Control value = {user.City} onChange = {getUserData}/>
       </Form.Group>
   
       <Form.Group as={Col} controlId="formGridState">
         <Form.Label>State</Form.Label>
-        <Form.Select defaultValue="Choose...">
+        <Form.Select defaultValue="Choose..." value = {user.State} onChange = {getUserData}>
           <option>Choose...</option>
-          <option>...</option>
+          <option>Haryana</option>
+          <option>Delhi</option>
+          <option>Rajasthan</option>
         </Form.Select>
       </Form.Group>
   
       <Form.Group as={Col} controlId="formGridZip">
         <Form.Label>Zip</Form.Label>
-        <Form.Control />
+        <Form.Control value = {user.Zip} onChange = {getUserData}/>
       </Form.Group>
     </Row>
     
     <Form.Group controlId="formFile" className="mb-3">
     <Form.Label>Upload your resume</Form.Label>
-    <Form.Control type="file" />
+    <Form.Control type="file"/>
     </Form.Group>
     <Form.Group className="mb-3" controlId="formGridAddress1">
       <Form.Label>Preference</Form.Label>
@@ -116,7 +153,7 @@ class SimpleForm extends Component {
             </div>
             ))}
             </Form.Group>
-    <Button variant="primary" type="submit" size = 'xxl'>
+    <Button variant="primary" type="submit" size = 'xxl' onClick = {postData}>
       Submit
     </Button>
   </Form>
@@ -124,6 +161,4 @@ class SimpleForm extends Component {
   </Container>
    )    
 }
-}
 
-export default SimpleForm;
